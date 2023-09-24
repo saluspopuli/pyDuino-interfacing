9fc4e7c47dc55323c2baeb4a2e5a0df9595bfd19
saluspopuli <andrecortez539@gmail.com>
2023-09-24 23:05:42 +0800

    Finished test code

--- Main.py
+++ Main.py
@@ -2,15 +2,17 @@ import serial
 import GUI as GUI
 
 def init_serial(comport, baudrate):
-    ser = serial.Serial(comport, baudrate, timeout=0.1)         # 1/timeout is the frequency at which the port is read
+    ser = serial.Serial(comport, baudrate, timeout=0.1)
     return ser
 
 def check_serial_data():
     while True:
         arduino_data = ser.readline().decode().strip().split("-")
         print(arduino_data)
-        if arduino_data[0] != '' and arduino_data[1] != '':
-            gui.move_label(arduino_data[0],arduino_data[1])
+
+        if len(arduino_data) >= 2:
+            if arduino_data[0] != '' and arduino_data[1] != '':
+                gui.move_label(arduino_data[0],arduino_data[1])
 
 if __name__ == '__main__':
 
